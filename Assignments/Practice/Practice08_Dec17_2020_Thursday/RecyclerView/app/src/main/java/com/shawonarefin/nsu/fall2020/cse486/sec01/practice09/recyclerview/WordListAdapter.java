987edb8634a75a26480af0be0810a2c04da7a080
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2020-12-17 14:06:49 +0600

    Added LinkedList and LayoutInflater to hold data in adapter:
    Added LinkedList and LayoutInflater to hold data in adapter.

--- Assignments/Practice/Practice08_Dec17_2020_Thursday/RecyclerView/app/src/main/java/com/shawonarefin/nsu/fall2020/cse486/sec01/practice09/recyclerview/WordListAdapter.java
+++ Assignments/Practice/Practice08_Dec17_2020_Thursday/RecyclerView/app/src/main/java/com/shawonarefin/nsu/fall2020/cse486/sec01/practice09/recyclerview/WordListAdapter.java
@@ -1,13 +1,24 @@
 package com.shawonarefin.nsu.fall2020.cse486.sec01.practice09.recyclerview;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList){
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
 
     @NonNull
     @Override
@@ -22,7 +33,7 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mWordList.size();
     }
 
     public class WordViewHolder extends RecyclerView.ViewHolder {
