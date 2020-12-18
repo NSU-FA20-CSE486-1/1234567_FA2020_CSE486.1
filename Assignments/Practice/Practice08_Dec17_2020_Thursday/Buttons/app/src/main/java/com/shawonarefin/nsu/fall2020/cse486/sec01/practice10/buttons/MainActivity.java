b34d10d3a0611d257c224495046bb9f5992f6772
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2020-12-18 14:17:04 +0600

    Button Test Application
    Practicing various event listener interface with buttons in Buttons Application.

--- Assignments/Practice/Practice08_Dec17_2020_Thursday/Buttons/app/src/main/java/com/shawonarefin/nsu/fall2020/cse486/sec01/practice10/buttons/MainActivity.java
+++ Assignments/Practice/Practice08_Dec17_2020_Thursday/Buttons/app/src/main/java/com/shawonarefin/nsu/fall2020/cse486/sec01/practice10/buttons/MainActivity.java
@@ -3,21 +3,45 @@ package com.shawonarefin.nsu.fall2020.cse486.sec01.practice10.buttons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button regularButton;
+    private ImageButton imageButton;
+    private Button borderlessButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        regularButton = findViewById(R.id.regular_button);
+        regularButton.setOnClickListener(view -> {
+            Toast.makeText(view.getContext(), "Clicked on Regular Button", Toast.LENGTH_SHORT).show();
+        });
+        regularButton.setOnLongClickListener(view -> {
+            Toast.makeText(view.getContext(), "Long press on Regular Button", Toast.LENGTH_LONG).show();
+            return true;
+        });
 
-    public void handleImageButton(View view){
-        Toast.makeText(this, "Clicked on Image Button", Toast.LENGTH_SHORT).show();
-    }
+        imageButton = findViewById(R.id.clock_button);
+        imageButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Clicked on Image Button", Toast.LENGTH_SHORT).show();
+        });
+        imageButton.setOnLongClickListener(view -> {
+            Toast.makeText(view.getContext(), "Long press on Image Button", Toast.LENGTH_LONG).show();
+            return true;
+        });
 
-    public void handleFlatButton(View view){
-        Toast.makeText(this, "Clicked on Flat Button", Toast.LENGTH_SHORT).show();
+        borderlessButton = findViewById(R.id.borderless_button);
+        borderlessButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Clicked on Flat Button", Toast.LENGTH_SHORT).show();
+        });
+        borderlessButton.setOnLongClickListener(view -> {
+            Toast.makeText(view.getContext(), "Long press on Flat Button", Toast.LENGTH_LONG).show();
+            return true;
+        });
     }
 }
