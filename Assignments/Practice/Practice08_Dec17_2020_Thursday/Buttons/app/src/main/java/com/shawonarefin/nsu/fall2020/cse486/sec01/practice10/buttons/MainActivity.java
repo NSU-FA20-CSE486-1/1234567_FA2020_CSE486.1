937cb08acabc9fa51663bae5df71ae541897993e
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2020-12-18 15:25:00 +0600

    Button Test Application
    Practicing various event listener interface with buttons in Buttons Application. Explored On Touch Listener.

--- Assignments/Practice/Practice08_Dec17_2020_Thursday/Buttons/app/src/main/java/com/shawonarefin/nsu/fall2020/cse486/sec01/practice10/buttons/MainActivity.java
+++ Assignments/Practice/Practice08_Dec17_2020_Thursday/Buttons/app/src/main/java/com/shawonarefin/nsu/fall2020/cse486/sec01/practice10/buttons/MainActivity.java
@@ -2,9 +2,11 @@ package com.shawonarefin.nsu.fall2020.cse486.sec01.practice10.buttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,36 +14,78 @@ public class MainActivity extends AppCompatActivity {
     private Button regularButton;
     private ImageButton imageButton;
     private Button borderlessButton;
+    private TextView displayActionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayActionTextView = findViewById(R.id.display_action_textview);
+
         regularButton = findViewById(R.id.regular_button);
         regularButton.setOnClickListener(view -> {
-            Toast.makeText(view.getContext(), "Clicked on Regular Button", Toast.LENGTH_SHORT).show();
+            displayActionTextView.setText("Clicked on Regular Button");
         });
         regularButton.setOnLongClickListener(view -> {
-            Toast.makeText(view.getContext(), "Long press on Regular Button", Toast.LENGTH_LONG).show();
+            displayActionTextView.setText("Long press on Regular Button");
             return true;
+        });
+        regularButton.setOnTouchListener((view, motionEvent) -> {
+            boolean returnValue = false;
+            switch (motionEvent.getAction()){
+                case MotionEvent.ACTION_MOVE:
+                    displayActionTextView.setText("Regular button touched : " + motionEvent.ACTION_MOVE);
+                    returnValue = true;
+                    break;
+                default:
+                    returnValue = false;
+            }
+
+            return returnValue;
         });
 
         imageButton = findViewById(R.id.clock_button);
         imageButton.setOnClickListener(view -> {
-            Toast.makeText(this, "Clicked on Image Button", Toast.LENGTH_SHORT).show();
+            displayActionTextView.setText("Clicked on Image Button");
         });
         imageButton.setOnLongClickListener(view -> {
-            Toast.makeText(view.getContext(), "Long press on Image Button", Toast.LENGTH_LONG).show();
+            displayActionTextView.setText("Long press on Image Button");
             return true;
+        });
+        imageButton.setOnTouchListener((view, motionEvent) -> {
+            boolean returnValue = false;
+            switch (motionEvent.getAction()){
+                case MotionEvent.ACTION_MOVE:
+                    displayActionTextView.setText("Image button touched : " + motionEvent.ACTION_MOVE);
+                    returnValue = true;
+                    break;
+                default:
+                    returnValue = false;
+            }
+
+            return returnValue;
         });
 
         borderlessButton = findViewById(R.id.borderless_button);
         borderlessButton.setOnClickListener(view -> {
-            Toast.makeText(this, "Clicked on Flat Button", Toast.LENGTH_SHORT).show();
+            displayActionTextView.setText("Clicked on Flat Button");
         });
         borderlessButton.setOnLongClickListener(view -> {
-            Toast.makeText(view.getContext(), "Long press on Flat Button", Toast.LENGTH_LONG).show();
+            displayActionTextView.setText("Borderless button long pressed");
             return true;
+        });
+        borderlessButton.setOnTouchListener((view, motionEvent) -> {
+            boolean returnValue = false;
+            switch (motionEvent.getAction()){
+                case MotionEvent.ACTION_MOVE:
+                    displayActionTextView.setText("Borderless button touched : " + motionEvent.ACTION_MOVE);
+                    returnValue = true;
+                    break;
+                default:
+                    returnValue = false;
+            }
+
+            return returnValue;
         });
     }
 }
